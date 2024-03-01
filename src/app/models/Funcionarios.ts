f1d9b7b36921c3bb683fd99df9b8f1fb0145f09f
amorimfe7 <amorimfe7@gmail.com>
2024-03-01 12:44:33 -0300

    criando Models

---
+++ src/app/models/Funcionarios.ts
@@ -0,0 +1,10 @@
+export interface Funcionarios {
+  id?: number;
+  nome: string;
+  sobrenome: string;
+  departamento: string;
+  status: boolean;
+  turno: string;
+  dataDeCriacao?: string;
+  dataDeAlteracao?: string;
+}
