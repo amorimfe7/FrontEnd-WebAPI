76a731cd5bcc33de276e65821974951ab0436b4c
amorimfe7 <amorimfe7@gmail.com>
2024-03-03 17:51:36 -0300

    criando requisição do GetFuncionarios

--- src/app/services/funcionario.service.ts
+++ src/app/services/funcionario.service.ts
@@ -1,9 +1,20 @@
+import { HttpClient, HttpClientModule } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { environment } from '../../environments/environment.development';
+import { Observable } from 'rxjs';
+import { Funcionario } from '../models/Funcionarios';
+import { Response } from '../models/Response';
 
 @Injectable({
   providedIn: 'root'
 })
 export class FuncionarioService {
 
-  constructor() { }
+  private urlAPI = `${environment.urlAPI}api/Funcionario`
+  constructor(private http: HttpClient) { }
+
+  GetFuncionarios(): Observable<Response<Funcionario[]>> {
+    return this.http.get<Response<Funcionario[]>>(this.urlAPI);
+  }
+
 }
