73be5499ab8b3c92e4ce20a8f47f0056fe9e6144
amorimfe7 <amorimfe7@gmail.com>
2024-03-11 18:22:28 -0300

    captando ID do Funcionário via rota e armazenando retorno do GetFuncionariosById

--- src/app/pages/editar/editar.component.ts
+++ src/app/pages/editar/editar.component.ts
@@ -1,12 +1,28 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { FuncionarioService } from '../../services/funcionario.service';
+import { ActivatedRoute, Router } from '@angular/router';
+import { Funcionarios } from '../../models/Funcionarios';
 
 @Component({
   selector: 'app-editar',
   templateUrl: './editar.component.html',
   styleUrl: './editar.component.css'
 })
-export class EditarComponent {
+export class EditarComponent implements OnInit{
   tituloPage: string = "Editar um Funcionário"
   btnAcao: string = "Editar"
-  
+  funcionario!: Funcionarios
+
+  constructor(private funcionarioService : FuncionarioService, private route: ActivatedRoute, private router: Router){}
+
+  ngOnInit(): void {
+      const id = Number(this.route.snapshot.paramMap.get('id'));
+
+      this.funcionarioService.GetFuncionariosById(id).subscribe((data =>{
+        this.funcionario = data.dados;
+
+        console.log(this.funcionario);
+      }))
+  }
+
 }
