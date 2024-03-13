f63718356455deed0acbc3af30629724b82079c5
amorimfe7 <amorimfe7@gmail.com>
2024-03-13 14:53:43 -0300

    criando component Detalhes e direcionando botão p/ página

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,11 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './pages/home/home.component';
 import { CadastroComponent } from './pages/cadastro/cadastro.component';
 import { EditarComponent } from './pages/editar/editar.component';
+import { DetalhesComponent } from './pages/detalhes/detalhes.component';
 
 const routes: Routes = [
   {path:'' , component:HomeComponent},
   {path:'cadastro', component:CadastroComponent},
-  {path:'editar/:id', component:EditarComponent}
+  {path:'editar/:id', component:EditarComponent},
+  {path: 'detalhes/:id', component:DetalhesComponent}
 ];
 
 @NgModule({
